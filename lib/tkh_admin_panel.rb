666a3d09795f34d0eb75549b461b405fad57d7ed
Swami Atma <swami@ohlalaweb.com>
2012-07-24 17:17:09 +0200

    Undoing things which don't work.

--- lib/tkh_admin_panel.rb
+++ lib/tkh_admin_panel.rb
@@ -4,8 +4,6 @@ module TkhAdminPanel
   # I don't think this is necessary
   # module Rails
     class Engine < ::Rails::Engine
-      require 'bootstrap'
-      require 'bootstrap-responsive'
     end
   # end
 end
