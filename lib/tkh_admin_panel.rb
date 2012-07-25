77cab7b0c9103543ccb10f78329063ffe1e763c8
Swami Atma <swami@ohlalaweb.com>
2012-07-25 10:28:34 +0200

    Trying to trigger precompile of admin assets in gem itself.

--- lib/tkh_admin_panel.rb
+++ lib/tkh_admin_panel.rb
@@ -2,9 +2,9 @@ require "tkh_admin_panel/version"
 require 'bootstrap-sass'
 
 module TkhAdminPanel
-  # I don't think this is necessary
-  # module Rails
-    class Engine < ::Rails::Engine
+  class Engine < ::Rails::Engine
+    initializer "TkhAdminPanel precompile hook", :group => :all do |app|
+      app.config.assets.precompile += ['admin.js', 'admin.css']
     end
-  # end
+  end
 end
