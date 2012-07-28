da426ee15520a2397b73781de58dd5a08e5bb7be
Swami Atma <swami@ohlalaweb.com>
2012-07-28 13:32:24 +0200

    Still working on language switcher.

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  
-  def full_language_name( lang = :en)
-    list = {:en => 'English', :fr => 'Français', :es => 'Español'}
-    list[lang]
-  end
-  helper_method :full_language_name
-
-end
