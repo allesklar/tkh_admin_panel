e034e2ce1a268d4e8ae99d520bb9549c5d083445
Swami Atma <swami@ohlalaweb.com>
2012-09-17 11:44:56 +0200

    Fixed a bug in create settings migration and populate table with one record.

--- lib/generators/tkh_admin_panel/create_or_update_migrations/templates/create_settings.rb
+++ lib/generators/tkh_admin_panel/create_or_update_migrations/templates/create_settings.rb
@@ -8,10 +8,21 @@ class CreateSettings < ActiveRecord::Migration
       t.boolean :disable_blog, default: false
       t.string :blog_name
       t.string :blog_tagline
-      t.boolean :enable_comments_in_pages, default: true
+      t.boolean :enable_comments_in_blog, default: true
       t.timestamps
     end
     Setting.create_translation_table! :site_name => :string, :site_tagline => :string, :blog_name => :string, :blog_tagline => :string
+    # normally it's not a good idea to populate a table in the migration but I think this is an exception
+    Setting.create([
+      { site_name: 'Name of the web site',
+        site_tagline: 'A short descriptive sentence',
+        enable_comments_in_pages: false,
+        disable_blog: false,
+        blog_name: 'Name of the blog',
+        blog_tagline: "A short descriptive sentence",
+        enable_comments_in_blog: true
+      }
+    ])
   end
   
   def self.down
