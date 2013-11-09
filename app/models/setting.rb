a0bdc2e9f6bf5b3f2f2ba17531db8adc82110392
Swami Atma <swami@ohlalaweb.com>
2013-11-09 10:19:04 +0100

    Disabled attr_accessible in setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,12 +1,12 @@
 # this is needed for now to make mass assignment security compatible with the translation of globalize3
-Globalize::ActiveRecord::Translation.class_eval do
-  attr_accessible :locale
-end
+# Globalize::ActiveRecord::Translation.class_eval do
+#   attr_accessible :locale
+# end
 
 class Setting < ActiveRecord::Base
-  
-  attr_accessible :company_name, :contact_email, :site_name, :site_tagline, :blog_name, :blog_tagline, :disable_blog, :enable_comments_in_pages, :enable_comments_in_blog
-  
+
+  # attr_accessible :company_name, :contact_email, :site_name, :site_tagline, :blog_name, :blog_tagline, :disable_blog, :enable_comments_in_pages, :enable_comments_in_blog
+
   translates :site_name, :site_tagline, :blog_name, :blog_tagline
-  
+
 end
