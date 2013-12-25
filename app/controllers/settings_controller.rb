94f6eb2f9969125c06d6977237dbf3249fbdf1c2
Swami Atma <swami@ohlalaweb.com>
2013-12-25 18:41:02 +0100

    Protecting attributes of settings resource.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -19,10 +19,17 @@ class SettingsController < ApplicationController
 
   def update
     @setting = Setting.first
-    if @setting.update_attributes(params[:setting])
+    if @setting.update_attributes(setting_params)
       redirect_to @setting, notice: t('settings.update.notice')
     else
       render action: "edit", warning: t('settings.update.warning')
     end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def setting_params
+    params.require(:setting).permit( :company_name, :contact_email, :site_name, :site_tagline, :blog_name, :blog_tagline, :disable_blog, :enable_comments_in_pages, :enable_comments_in_blog )
   end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,11 +1,4 @@
-# this is needed for now to make mass assignment security compatible with the translation of globalize3
-# Globalize::ActiveRecord::Translation.class_eval do
-#   attr_accessible :locale
-# end
-
 class Setting < ActiveRecord::Base
-
-  # attr_accessible :company_name, :contact_email, :site_name, :site_tagline, :blog_name, :blog_tagline, :disable_blog, :enable_comments_in_pages, :enable_comments_in_blog
 
   translates :site_name, :site_tagline, :blog_name, :blog_tagline
 
