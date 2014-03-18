74b713a5c7c056af0d5ab00f0d4f466edc6499cc
Swami Atma <swami@ohlalaweb.com>
2014-03-18 12:05:51 +0100

    Debugging assets pipeline.

--- lib/tkh_admin_panel.rb
+++ lib/tkh_admin_panel.rb
@@ -10,9 +10,10 @@ require 'jquery-ui-rails'
 
 module TkhAdminPanel
   class Engine < ::Rails::Engine
-    initializer "TkhAdminPanel precompile hook", :group => :all do |app|
-      app.config.assets.precompile += ['admin.js', 'admin.css', 'admin/pages.js', 'admin/illustrations.js', 'admin/menus.js']
-    end
+    # debugging. this is not working. moving directive to host app
+    # initializer "TkhAdminPanel precompile hook", :group => :all do |app|
+    #   app.config.assets.precompile += ['admin.js', 'admin.css', 'admin/pages.js', 'admin/illustrations.js', 'admin/menus.js']
+    # end
 
     # to extend the application_controller in the host app
     initializer 'tkh_admin_panel.controller' do |app|
