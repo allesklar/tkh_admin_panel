881ba80820c62459f25feed4873635a5251c759a
Swami Atma <swami@TenThousandHours.eu>
2015-08-15 12:28:57 +0200

    In the master branch, the normal route system should apply.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # The route_translator branch needs to be compatible with its
-  # namesake gem
-  localized do
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :settings
   end
+
 end
