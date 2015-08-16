23b88c436e72ee2b88c646e7f9c23284aa823616
Swami Atma <swami@TenThousandHours.eu>
2015-08-16 21:01:06 +0200

    Had to reset the route_translator route wrapper.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  # The route_translator branch needs to be compatible with its
+  # namesake gem
+  localized do
     resources :settings
   end
-
 end
